4810e07340050c6651262da3deda8cb565c01c71
Jochen Dolze <vdr@dolze.de>
2009-11-22 14:29:14 +0100

    Fixed translation bug reported from TomG Added italian translations from Diego Pierotto

--- markad.h
+++ markad.h
@@ -23,7 +23,7 @@ public:
   cPluginMarkAd(void);
   virtual ~cPluginMarkAd();
   virtual const char *Version(void) { return VERSION; }
-  virtual const char *Description(void) { return DESCRIPTION; }
+  virtual const char *Description(void) { return tr(DESCRIPTION); }
   virtual const char *CommandLineHelp(void);
   virtual bool ProcessArgs(int argc, char *argv[]);
   virtual bool Initialize(void);
