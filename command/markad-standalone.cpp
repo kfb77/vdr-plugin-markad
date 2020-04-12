3e96b7f48e41008bd2f59f4fea99ebfe1bce0d37
kfb77 <>
2020-04-12 18:35:09 +0200

    accept vborder stop as end of recording

--- command/markad-standalone.cpp
+++ command/markad-standalone.cpp
@@ -320,12 +320,17 @@ void cMarkAdStandalone::CheckStop()
             end=marks.GetAround(3*delta,iStopA,MT_HBORDERSTOP);         // try MT_HBORDERSTOP
             if (!end) {
                 dsyslog("no MT_HBORDERSTOP mark found");
-                end=marks.GetAround(3*delta,iStopA+2*delta,MT_LOGOSTOP);        // try MT_LOGOSTOP
+                end=marks.GetAround(3*delta,iStopA,MT_VBORDERSTOP);         // try MT_VBORDERSTOP
                 if (!end) {
-                    dsyslog("no MT_LOGOSTOP mark found");
-                    end=marks.GetAround(3*delta,iStopA,MT_STOP,0x0F);    // try any type of stop mark
+                    dsyslog("no MT_VBORDERSTOP mark found");
+                    end=marks.GetAround(3*delta,iStopA+2*delta,MT_LOGOSTOP);        // try MT_LOGOSTOP
+                    if (!end) {
+                        dsyslog("no MT_LOGOSTOP mark found");
+                        end=marks.GetAround(3*delta,iStopA,MT_STOP,0x0F);    // try any type of stop mark
+                    }
+                    else dsyslog("MT_LOGOSTOP found at frame %i", end->position);
                 }
-                else dsyslog("MT_LOGOSTOP found at frame %i", end->position);
+                else dsyslog("MT_VBORDERSTOP found at frame %i", end->position);
             }
             else dsyslog("MT_HBORDERSTOP found at frame %i", end->position);
         }
