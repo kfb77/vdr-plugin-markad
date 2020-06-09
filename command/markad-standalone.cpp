a227a74b360961b76ec5659865f386dde479b73f
kfb77 <>
2020-06-09 16:16:41 +0200

    accept later logo start

--- command/markad-standalone.cpp
+++ command/markad-standalone.cpp
@@ -683,7 +683,7 @@ void cMarkAdStandalone::CheckStart() {
 
     if (!begin) {    // try anything
         marks.DelTill(1);    // we do not want to have a start mark at position 0
-        begin=marks.GetAround(iStartA+delta,iStartA,MT_START,0x0F);
+        begin=marks.GetAround(iStartA+3*delta,iStartA,MT_START,0x0F);
         if (begin) {
             dsyslog("cMarkAdStandalone::CheckStart(): found start mark at (%i)", begin->position);
         }
