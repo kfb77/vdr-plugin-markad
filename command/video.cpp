df8b10fd3a3661560d7828bb639fddbbf77b0ebd
kfb77 <kfb77>
2020-09-30 09:49:30 +0200

    increase max brightness for reduce brightness function from 130 to 200

--- command/video.cpp
+++ command/video.cpp
@@ -444,7 +444,7 @@ int cMarkAdLogo::Detect(const int framenumber, int *logoframenumber, const bool 
     // if we only have one plane we are "vulnerable"
     // to very bright pictures, so ignore them...
     if (processed == 1) {
-        if ((area.intensity > 120) && (area.intensity < 130) && (rpixel < (mpixel * LOGO_IMARK))) {  // if we found no logo try to reduce brightness, if we are to bright, this will not work
+        if ((area.intensity > 120) && (area.intensity < 200) && (rpixel < (mpixel * LOGO_IMARK))) {  // if we found no logo try to reduce brightness, if we are to bright, this will not work
             tsyslog("cMarkAdLogo::Detect(): frame (%5d) to bright,     area intensity %d", framenumber, area.intensity);
             if (ReduceBrightness(framenumber)) {
                 area.rpixel[0] = 0;
