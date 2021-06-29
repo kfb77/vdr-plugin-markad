d3289c08d49fb77f076c64d12e788cbf3481d657
kfb77 <kfb77>
2021-06-29 22:32:51 +0200

    check blackscreen start mark for closing credits from previous recording

--- command/markad-standalone.cpp
+++ command/markad-standalone.cpp
@@ -1257,6 +1257,26 @@ void cMarkAdStandalone::CheckStart() {
         begin = NULL;
     }
 
+    // if we found a blackscreen start mark, check if we are in very long closing credit from previous recording
+    if (begin && (begin->type == MT_NOBLACKSTART)) {
+        bool isInvalid = true;
+        while (isInvalid) {
+            cMark *nextStop  = blackMarks.GetNext(begin->position, MT_NOBLACKSTOP);
+            cMark *nextStart = blackMarks.GetNext(begin->position, MT_NOBLACKSTART);
+            if (nextStart && nextStop) {
+                int diff = (nextStop->position - begin->position) / macontext.Video.Info.framesPerSecond;
+                int adLength = (nextStart->position - nextStop->position) / macontext.Video.Info.framesPerSecond;;
+                dsyslog("cMarkAdStandalone::CheckStart(): next blackscreen from (%d) to (%d) in %ds, length %ds", nextStop->position, nextStart->position, diff, adLength);
+                if ((diff <= 67) && (adLength >= 5)) {  // changed from 3 to 5, avoid long dark scenes
+                    dsyslog("cMarkAdStandalone::CheckStart(): very long blackscreen short after, we are in the closing credits of previous recording");
+                    begin = nextStart;
+                }
+                else isInvalid = false;
+            }
+            else isInvalid = false;
+        }
+    }
+
     if (begin) {
         marks.DelTill(begin->position, &blackMarks);    // delete all marks till start mark
         CalculateCheckPositions(begin->position);
