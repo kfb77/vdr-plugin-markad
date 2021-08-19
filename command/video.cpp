1f663eb0502cbeec3bbd700ac0064245a8d8e289
kfb77 <kfb77>
2021-08-19 16:57:09 +0200

    ReduceBrightness: increase min contrast for valid high contrast area

--- command/video.cpp
+++ command/video.cpp
@@ -615,10 +615,13 @@ int cMarkAdLogo::ReduceBrightness(__attribute__((unused)) const int frameNumber,
     }
 
     // very high contrast with not very high brightness in logo area, trust detection
-    // contrast 163, brightness 101
-    // contrast 159, brightness 112
-    if ((contrastLogo >= 159) && (brightnessLogo <= 124)) { // changed from 112 to 110 to 124
-	                                                    // increased to detect SIXX info logo as no logo
+    // logo is visible but not detected
+    // contrast 159, brightness 123
+    // contrast 161, brightness 120
+    // contrast 163, brightness 124
+    if ((contrastLogo >= 164) && (brightnessLogo <= 124)) { // changed contrastLogo from 159 to 164
+                                                            // changed brightnessLogo from 112 to 110 to 124
+                                                            // increased to detect SIXX info logo as no logo
 #ifdef DEBUG_LOGO_DETECTION
         dsyslog("cMarkAdLogo::ReduceBrightness(): very high contrast with not very high brightness in logo area, trust detection");
 #endif
