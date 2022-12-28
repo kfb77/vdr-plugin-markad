kfb77/vdr-plugin-markad

commit ccfb1a3be943648b663b5a86cba9755b41afa125

markad-standalone.cpp, backtrace() is posix only.

command/markad-standalone.cpp
@@ -4505,10 +4505,6 @@ int usage(int svdrpport) {
 
 
 static void signal_handler(int sig) {
-    void *trace[32];
-    char **messages = static_cast<char **>(NULL);
-    int i, trace_size = 0;
-
     switch (sig) {
         #ifdef POSIX
         case SIGTSTP:
@@ -4524,17 +4520,23 @@ static void signal_handler(int sig) {
             esyslog("aborted by signal");
             abortNow = true;;
             break;
-        case SIGSEGV:
+        case SIGSEGV: {
             esyslog("segmentation fault");
 
-            trace_size = backtrace(trace, 32);
-            messages = backtrace_symbols(trace, trace_size);
+            #ifdef POSIX
+            void *trace[32];
+            int trace_size = backtrace(trace, 32);
+            char **messages = backtrace_symbols(trace, trace_size);
+
             esyslog("[bt] Execution path:");
             for (i=0; i < trace_size; ++i) {
                 esyslog("[bt] %s", messages[i]);
             }
+            #endif /* #ifdef POSIX */
+
             _exit(1);
             break;
+            }
         case SIGTERM:
         case SIGINT:
             esyslog("aborted by user");

commit 3107e6557a1f0e8108acfa469ae842c7057b6ad1

markad-standalone.cpp: fix bug introduced in ccfb1a3

command/markad-standalone.cpp
@@ -4526,7 +4526,7 @@ static void signal_handler(int sig) {
 
             #ifdef POSIX
             void *trace[32];
-            int trace_size = backtrace(trace, 32);
+            int i, trace_size = backtrace(trace, 32);
             char **messages = backtrace_symbols(trace, trace_size);
 
             esyslog("[bt] Execution path:");