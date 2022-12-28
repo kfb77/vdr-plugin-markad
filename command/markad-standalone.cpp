kfb77/vdr-plugin-markad

commit c8da970eb50d6df502571efceed1dba65e1ab49d

markad-standalone.cpp: disable ioprio and syslog on win32

command/markad-standalone.cpp
@@ -51,7 +51,9 @@ int logoChangeTime_ms = 0;
 int decodeTime_us = 0;
 
 
-static inline int ioprio_set(int which, int who, int ioprio) {
+#ifdef POSIX
+
+static inline int static inline int ioprio_set(int which, int who, int ioprio) {(int which, int who, int ioprio) {
 #if defined(__i386__)
 #define __NR_ioprio_set         289
 #elif defined(__ppc__)
@@ -135,6 +137,7 @@ void syslog_with_tid(int priority, const char *format, ...) {
     }
 }
 
+#endif /* #ifdef POSIX */
 
 void cMarkAdStandalone::CalculateCheckPositions(int startframe) {
     dsyslog("cMarkAdStandalone::CalculateCheckPositions(): startframe %i (%dmin %2ds)", startframe, static_cast<int>(startframe / macontext.Video.Info.framesPerSecond / 60), static_cast<int>(startframe / macontext.Video.Info.framesPerSecond) % 60);

commit a4423712c5e8a07dcc5a69c3e3149b9fc27cffaa

'markad-standalone.cpp: fix bug introduced in commit c8da970

command/markad-standalone.cpp
@@ -53,7 +53,7 @@ int decodeTime_us = 0;
 
 #ifdef POSIX
 
-static inline int static inline int ioprio_set(int which, int who, int ioprio) {(int which, int who, int ioprio) {
+static inline int ioprio_set(int which, int who, int ioprio) {
 #if defined(__i386__)
 #define __NR_ioprio_set         289
 #elif defined(__ppc__)