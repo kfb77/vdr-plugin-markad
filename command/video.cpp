e6b896e810431892b1e7ed55e632a25f45933d1e
kfb77 <kfb77>
2023-04-07 14:42:56 +0200

    cMarkAdLogo::Detect(): reduce max intensity for colored logos

--- command/video.cpp
+++ command/video.cpp
@@ -1080,7 +1080,7 @@ int cMarkAdLogo::Detect(const int frameBefore, const int frameCurrent, int *logo
     }
     else {  // if we have more planes we can still have a problem with coloured logo on same colored background
         // too bright
-        if ((area.status == LOGO_VISIBLE) && (area.intensity >= 122) &&             // too bright, logo detection can be wrong, changed from 137 to 131 to 122
+        if ((area.status == LOGO_VISIBLE) && (area.intensity >= 117) &&             // too bright, logo detection can be wrong, changed from 122 to 117
             (rPixel > 0) && (rPixel < (mPixel * logo_imark))) return LOGO_NOCHANGE; // trust 0 matches
 
         // maybe coloured logo on same colored background, try without plane 0
