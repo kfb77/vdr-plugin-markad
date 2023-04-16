47c4e5678e0fd3b11c076afda0a7eee518bad9fa
kfb77 <kfb77>
2023-04-16 13:59:54 +0200

    cExtractLogo::SearchLogo(): reduce min valid hits

--- command/logo.cpp
+++ command/logo.cpp
@@ -1599,12 +1599,12 @@ int cExtractLogo::SearchLogo(sMarkAdContext *maContext, int startFrame) {  // re
         }
 
         if ((bestLogoCorner >= 0) &&
-           (((startFrame == 0) && ((bestLogoInfo.hits >= 14)) || // this is the very last try, use what we have, bettet than nothing
-                                  ((bestLogoInfo.hits >= 6) && (sumHits <= bestLogoInfo.hits + 1))) ||
+           (((startFrame == 0) && ((bestLogoInfo.hits >= 14)) || // this is the very last try, use what we have, bettet than nothing, chaned from 14 to 8
+                                  ((bestLogoInfo.hits >= 2) && (sumHits <= bestLogoInfo.hits + 1))) ||  // changed from 6 to 2
              (bestLogoInfo.hits >= 50) || // we have a good result
             ((bestLogoInfo.hits >= 40) && (sumHits <= bestLogoInfo.hits + 10)) ||  // if most hits are in the same corner than less are enough
-            ((bestLogoInfo.hits >= 30) && (sumHits <= bestLogoInfo.hits + 3)) ||  // if almost all hits are in the same corner than less are enough
-            ((bestLogoInfo.hits >= 10) && (sumHits == bestLogoInfo.hits)))) {  // if all hits are in the same corner than less are enough
+            ((bestLogoInfo.hits >= 30) && (sumHits <= bestLogoInfo.hits + 7)) ||   // if almost all hits are in the same corner than less are enough
+            ((bestLogoInfo.hits >= 10) && (sumHits == bestLogoInfo.hits)))) {      // if all hits are in the same corner than less are enough
             int secondLogoHeight = logoHeight;
             int secondLogoWidth = logoWidth;
             dsyslog("cExtractLogo::SearchLogo(): best corner is %s at frame %d with %d similars", aCorner[bestLogoCorner], bestLogoInfo.iFrameNumber, bestLogoInfo.hits);
