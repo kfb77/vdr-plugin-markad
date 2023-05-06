0c3a457669bd0c08e2663e63a580d7b661e44b13
kfb77 <kfb77>
2023-05-06 16:54:55 +0200

    cMarkAdLogo::Detect(): detect background pattern to prevent false logo detection

--- command/video.cpp
+++ command/video.cpp
@@ -859,7 +859,24 @@ int cMarkAdLogo::Detect(const int frameBefore, const int frameCurrent, int *logo
     // to very bright pictures, so ignore them...
     int brightnessState = BRIGHTNESS_UNINITIALIZED;
     int contrastReduced = -1;
-    if (processed == 1) {
+    if (processed == 1) {   // we have only 1 plane (no coloured logo
+
+        // prevent to detect background patten as logo
+        if ((area.status == LOGO_INVISIBLE) && (rPixel > (mPixel * logo_vmark))) {  // possible state change from invisible to visible
+            int black = 0;
+            for (int i = 0; i < logoHeight * logoWidth; i++) {
+                if (area.sobel[0][i] == 0) black++;
+            }
+            int quote = 100 * black / (logoHeight * logoWidth);
+#ifdef DEBUG_LOGO_DETECTION
+            dsyslog("cMarkAdLogo::Detect(): frame (%6d) pixel quote: %d%%", frameCurrent, quote);
+#endif
+            if (quote > 27) return LOGO_NOCHANGE; // there is a pattern on the backbround, no logo detection possible
+                                                  // changed from 22 to 27
+                                                  // do not reduce, we need to detect logo on trees or gras background
+        }
+
+        // check area intensitiy
 #define MAX_AREA_INTENSITY 75  // change from 128 to 127 to 126 to 125 to 114 to 100 to 98 to 94 to 80 to 75
                                // notice: there can be very bright logo parts in dark areas, this will result in a lower brightness
                                // we handle this cases in ReduceBrightness() when we detect contrast
@@ -908,6 +925,7 @@ int cMarkAdLogo::Detect(const int frameBefore, const int frameCurrent, int *logo
                     }
                 }
 #endif
+                // check background pattern
                 int black = 0;
                 for (int i = 0; i < logoHeight * logoWidth; i++) {
                     if (area.sobel[0][i] == 0) black++;
@@ -916,8 +934,10 @@ int cMarkAdLogo::Detect(const int frameBefore, const int frameCurrent, int *logo
 #ifdef DEBUG_LOGO_DETECTION
                 dsyslog("cMarkAdLogo::Detect(): frame (%6d) pixel quote after brighness reduction: %d%%", frameCurrent, quote);
 #endif
-                if (quote >= 22) return LOGO_NOCHANGE; // there is a pattern on the backbround, no logo detection possible, changed from 27 to 22
+                if (quote > 22) return LOGO_NOCHANGE; // there is a pattern on the backbround, no logo detection possible
+                                                      // changed from 27 to 25
 
+                // check new brightness
                 if (((area.intensity >= 152) && (rPixel >  0)) || // still too bright, we can not use the result
                     ((area.intensity >= 208) && (rPixel == 0))) {
 #ifdef DEBUG_LOGO_DETECTION
