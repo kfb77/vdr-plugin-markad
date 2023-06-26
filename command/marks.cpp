0bbcca05c1cb585ef205692deb9596a805c0fcbd
kfb77 <kfb77>
2023-06-26 23:18:05 +0200

    cMarks::DelInvalidSequence(): fix log message

--- command/marks.cpp
+++ command/marks.cpp
@@ -196,7 +196,7 @@ void cMarks::DelInvalidSequence() {
 
         // if first mark is a stop mark, remove it
         if (((mark->type & 0x0F) == MT_STOP) && (mark == first)){
-            dsyslog("cMarks::DelInvalidSequence(): Start with STOP mark (%d) type %d, delete first mark", mark->position, mark->type);
+            dsyslog("cMarks::DelInvalidSequence(): Start with STOP mark (%d) type 0x%X, delete first mark", mark->position, mark->type);
             cMark *tmp = mark;
             mark = mark->Next();
             Del(tmp);
@@ -206,7 +206,7 @@ void cMarks::DelInvalidSequence() {
         // cleanup of start followed by start or stop followed by stop
         if ((((mark->type & 0x0F) == MT_STOP)  && (mark->Next()) && ((mark->Next()->type & 0x0F) == MT_STOP)) || // two stop or start marks, keep strong marks, delete weak
             (((mark->type & 0x0F) == MT_START) && (mark->Next()) && ((mark->Next()->type & 0x0F) == MT_START))) {
-            dsyslog("cMarks::DelInvalidSequence(): mark (%d) type %d, followed by same mark (%d) type %d", mark->position, mark->type, mark->Next()->position, mark->Next()->type);
+            dsyslog("cMarks::DelInvalidSequence(): mark (%d) type 0x%X, followed by same mark (%d) type 0x%X", mark->position, mark->type, mark->Next()->position, mark->Next()->type);
             if (mark->type < mark->Next()->type) {
                 dsyslog(" cMarks::DelInvalidSequence() delete mark (%d)", mark->position);
                 cMark *tmp = mark;
