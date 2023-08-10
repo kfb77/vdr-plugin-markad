c2e9194812cca4fb5f9736da8699d8c2abb47e5e
kfb77 <kfb77>
2023-08-10 18:34:17 +0200

    cMarkAdStandalone::CheckMarks(): reduce min length of ad for final start mark check

--- command/markad-standalone.cpp
+++ command/markad-standalone.cpp
@@ -2111,11 +2111,12 @@ void cMarkAdStandalone::CheckMarks(const int endMarkPos) {           // cleanup 
         if (firstStop) {
             cMark *secondStart = marks.GetNext(firstStop->position, MT_LOGOSTART);
             if (secondStart) {
-                int lengthFirstAd       = (secondStart->position - firstStop->position) / macontext.Video.Info.framesPerSecond;
+                int lengthFirstAd       = 1000 * (secondStart->position - firstStop->position) / macontext.Video.Info.framesPerSecond;
                 int newDiffAfterAssumed = (secondStart->position - iStart)              / macontext.Video.Info.framesPerSecond;
-                dsyslog("cMarkAdStandalone::CheckMarks(): first logo stop mark (%d), next logo start mark (%d), length ad %ds, start %ds after assumed start", firstStop->position, secondStart->position, lengthFirstAd, newDiffAfterAssumed);
-                if ((lengthFirstAd >= 1)  && (lengthFirstAd <= 41) && (newDiffAfterAssumed <= 387)) {  // changed from 365 to 387
-                                                                                                       // very short first ad is logo detection failure
+                dsyslog("cMarkAdStandalone::CheckMarks(): first logo stop mark (%d), next logo start mark (%d), length ad %dms, start %ds after assumed start", firstStop->position, secondStart->position, lengthFirstAd, newDiffAfterAssumed);
+                if ((lengthFirstAd >= 800)  && (lengthFirstAd <= 41000) && (newDiffAfterAssumed <= 387)) {  // changed from 365 to 387
+                                                                                                            // changed from 840 to 800
+                                                                                                            // very short first ad is logo detection failure
                     dsyslog("cMarkAdStandalone::CheckMarks(): first ad too short for in broadcast, delete start (%d) and stop (%d) mark", firstStart->position, firstStop->position);
                     marks.Del(firstStart->position);
                     marks.Del(firstStop->position);
