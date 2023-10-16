001a4d77cdacf4bd3f9ae27d4c8bdd3ecb155776
kfb77 <kfb77>
2023-10-16 19:23:45 +0200

    cMarkAdStandalone::LoadInfo(): ignore VPS stop events results in too short broadcast

--- command/markad-standalone.cpp
+++ command/markad-standalone.cpp
@@ -5268,7 +5268,7 @@ bool cMarkAdStandalone::LoadInfo() {
                         int lengthVPS = vpsStop - macontext.Info.tStart;
                         int diff      = lengthVPS - length;
                         dsyslog("cMarkAdStandalone::LoadInfo(): broadcast length from VPS events:    %5ds -> %d:%02d:%02dh, %ds longer than length from vdr info file", lengthVPS, lengthVPS / 3600, (lengthVPS % 3600) / 60, length % 60, diff);
-                        if (abs(diff) >= 506) {
+                        if ((diff >= 506) || (diff < -570)) {  // changed from -284 to -570
                             dsyslog("cMarkAdStandalone::LoadInfo(): VPS stop event seems to be invalid, use length from vdr info file");
                             vps->SetStop(-1);  // set VPS stop event to invalid
                         }
