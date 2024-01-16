e43c5130d682ece28e2d0a8f78c2fb2dc9977f32
kfb77 <kfb77>
2024-01-16 16:36:20 +0100

    cDetectLogoStopStart::AdInFrameWithLogo(): reduce min length

--- command/evaluate.cpp
+++ command/evaluate.cpp
@@ -1565,7 +1565,7 @@ int cDetectLogoStopStart::AdInFrameWithLogo(const bool isStartMark) {
 #define AD_IN_FRAME_STOP_OFFSET_MAX  15479  // changed from 15800 to 15479 because of false detection, do not increase, accept missing detection of preview direct after ad in frame without logo interuption
 #define AD_IN_FRAME_START_OFFSET_MAX  4319  // changed from 4799 to 4319
 #define AD_IN_FRAME_LENGTH_MAX       34680  // changed from 30720 to 34680
-#define AD_IN_FRAME_LENGTH_MIN        6960  // shortest ad in frame found 6960ms, changed from 8880 to 6960
+#define AD_IN_FRAME_LENGTH_MIN        6920  // shortest ad in frame found 6920ms, changed from 6960 to 6920
     // prevent to get additional info logo as frame
     int isCornerLogo[CORNERS] = {0};
     int countFrames           =  0;
