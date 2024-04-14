04128efcf072475d825d8e5ba25efa68785dac7e
kfb77 <kfb77>
2024-04-14 13:16:08 +0200

    cMarkAdStandalone::Check_LOGOSTOP(): increase search range

--- command/markad-standalone.cpp
+++ command/markad-standalone.cpp
@@ -499,7 +499,9 @@ cMark *cMarkAdStandalone::Check_LOGOSTOP() {
         int diffAssumed = (iStopA - lEnd->position) / macontext.Video.Info.framesPerSecond;
         LogSeparator(false);
         dsyslog("cMarkAdStandalone::Check_LOGOSTOP(): check for separator for logo stop (%d), %ds before assumed end (%d)", lEnd->position, diffAssumed, iStopA);
-        if (diffAssumed >= 314) break;   // changed from 349 to 314
+        // examples:
+        // valid: 330 before iStartA
+        if (diffAssumed > 330) break;
         if (HaveBlackSeparator(lEnd) || HaveSilenceSeparator(lEnd) || HaveInfoLogoSequence(lEnd)) {
             end = lEnd;
         }
