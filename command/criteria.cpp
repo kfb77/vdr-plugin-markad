0b099a166f0b0859f4f1386ef1e673bb90ff8dc8
kfb77 <kfb77>
2024-05-03 15:57:25 +0200

    cCriteria::LogoFadeInOut(): add channel

--- command/criteria.cpp
+++ command/criteria.cpp
@@ -38,11 +38,15 @@ int cCriteria::LogoFadeInOut(const char *channelName) {
     if (CompareChannelName(channelName, "KiKA",           IGNORE_HD)) return FADE_IN | FADE_OUT;
     if (CompareChannelName(channelName, "Nickelodeon",    IGNORE_HD)) return FADE_IN | FADE_OUT;
     if (CompareChannelName(channelName, "NICK_MTV+",      IGNORE_HD)) return FADE_IN | FADE_OUT;
+    if (CompareChannelName(channelName, "Pro7_MAXX",      IGNORE_HD)) return FADE_IN;
+    if (CompareChannelName(channelName, "ProSieben_MAXX", IGNORE_HD)) return FADE_IN;
+    if (CompareChannelName(channelName, "sixx",           IGNORE_HD)) return FADE_IN;
     if (CompareChannelName(channelName, "SPORT1",         IGNORE_HD)) return FADE_IN | FADE_OUT;
     if (CompareChannelName(channelName, "TELE_5",         IGNORE_HD)) return FADE_IN | FADE_OUT;
     if (CompareChannelName(channelName, "TLC",            IGNORE_HD)) return FADE_IN | FADE_OUT;
     if (CompareChannelName(channelName, "VOX",            IGNORE_HD)) return FADE_OUT;             // very short fade out
     if (CompareChannelName(channelName, "VOXup",          IGNORE_HD)) return FADE_IN | FADE_OUT;   // very short fade out
+    if (CompareChannelName(channelName, "WELT",           IGNORE_HD)) return FADE_IN;
     if (CompareChannelName(channelName, "ZDF",            IGNORE_HD)) return FADE_IN | FADE_OUT;
 
     return FADE_NONE;
