50f8f4a8f8458bf31f09ba7644af601c55594888
kfb77 <kfb77>
2024-06-11 18:56:23 +0200

    cMarkAdStandalone::CheckStop(): allow early logo stop before aspect ratio start of next broadcast

--- command/markad-standalone.cpp
+++ command/markad-standalone.cpp
@@ -1426,7 +1426,7 @@ int cMarkAdStandalone::CheckStop() {
                     dsyslog("cMarkAdStandalone::CheckStop(): found %s stop mark (%d) %ds before aspect ratio end mark (%d), %ds before assumed stop", markType, stopBefore->position, diffAspectStop, end->position, diffStopA);
                     FREE(strlen(markType)+1, "text");
                     free(markType);
-                    if ((diffStopA <= 760) && (diffAspectStop <= 66)) { // changed from 682 to 760, for broadcast length from info file too long
+                    if ((diffStopA <= 867) && (diffAspectStop <= 66)) { // changed from 760 to 867, for broadcast length from info file too long
                         // changed from 39 to 40 to 66, for longer ad found between broadcasts
                         dsyslog("cMarkAdStandalone::CheckStop(): there is an advertising before aspect ratio change, use stop mark (%d) before as end mark", stopBefore->position);
                         end = stopBefore;
