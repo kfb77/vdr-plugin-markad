90bf17b523b0e3e33bca3dc7bda5c02fd2c22b2a
kfb77 <kfb77>
2024-10-24 15:19:27 +0200

    cMarks::Add(): keep vborder and logo start on same position

--- command/marks.cpp
+++ command/marks.cpp
@@ -438,6 +438,12 @@ cMark *cMarks::Add(const int type, const int oldType, const int newType, int pos
         dsyslog("cMarks::Add(): very short black screen, move MT_NOBLACKSTART to (%d)", position);
         dupMark = Get(position);  // now check duplicate with new position
     }
+    // move vborder start after logo start mark, need both to detect valid vborder start mark for channel with logo in border
+    else if (dupMark && (type == MT_VBORDERSTART) && (dupMark->type == MT_LOGOSTART)) {
+        position = index->GetPacketNumberAfter(position);
+        dsyslog("cMarks::Add(): start vborder and start logo on same position, move vborder start to (%d)", position);
+        dupMark = Get(position);  // now check duplicate with new position
+    }
     if (dupMark) {
         if ((type & 0xF0) != MT_SCENECHANGE) dsyslog("cMarks::Add(): duplicate mark on position (%d) type 0x%X and type 0x%X", position, type, dupMark->type); // duplicate scene changes happens frequently
 #ifdef DEBUG_SCENECHANGE
