8389104a8ad4e548cf151c0632800e0906b9f9e8
kfb77 <kfb77>
2025-06-14 14:27:13 +0200

    cCriteria::GoodVPS(): update channel list

--- command/criteria.cpp
+++ command/criteria.cpp
@@ -25,6 +25,7 @@ const char *cCriteria::GetChannelName() const {
 
 bool cCriteria::GoodVPS() {
     if (!channelName) return false;
+    if (CompareChannelName(channelName, "ARD_alpha",    IGNORE_HD))               return true;
 //    if (CompareChannelName(channelName, "arte",         IGNORE_HD))               return true;    // VPS start event before preview
     if (CompareChannelName(channelName, "Das_Erste",    IGNORE_HD))               return true;
     if (CompareChannelName(channelName, "hr-fernsehen", IGNORE_HD))               return true;
